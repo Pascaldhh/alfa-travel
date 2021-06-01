4155cd11d9891ae44cc167d1cf56255368d01d5c
hiddencorner <hoekhidde12@gmail.com>
2021-06-01 14:28:06 +0200

    footer + header
    added a footer and a header

--- footer.php
+++ footer.php
@@ -1,3 +1,6 @@
-    
+	<div class="vierkant" style="background-color:#003581;">
+	</div>
+	<div class="driehoek" style="transform:rotate(180deg);" "background-color:#003581;">
+	</div>    
 </body>
 </html>
