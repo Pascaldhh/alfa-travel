c38b5e757db279d731a5965164d333631db02fbf
hiddencorner <hoekhidde12@gmail.com>
2021-06-04 10:12:39 +0200

    finished footer and added browser tab icon for every platform
    footer & header still need menu buttons.

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
-	<div class="vierkant" style="background-color:#003581; height: 120px;">
+	<div class="vierkant" style="background-color:#003581; height: 180px;">
+		<div class="center">
+			<p>Pagina 1 | Pagina 2 | Pagina 3</p>
+		</div>
 	</div>
 	<div class="driehoekFooter">
-	</div>    
+	</div>   
 </body>
 </html>
--- header.php
+++ header.php
@@ -5,16 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Alfa-Travel Home-Pagina</title>
+    <!-- browser tab icon -->
+   	<link rel="apple-touch-icon" sizes="120x120" href="icon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="icon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="icon/favicon-16x16.png">
+	<link rel="manifest" href="icon/site.webmanifest">
+	<link rel="mask-icon" href="icon/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
 </head>
-<body>
-	<div clas="container">
+<body>	
+	<header>
 		<div class="driehoek">
 		</div>
 		<div class="vierkant">
 		</div>
 		<img src="images/logo.jpg">
-
-		<p>geen idee man, geen enkele css verandering die ik maakte deed het.. voelt alsof de css file die gebruikt word ergens verstopt is ofzo,. k kijk vrijdag ochtend verder, iig uren verspilt en ik ben boos</p>
-	</div>
-	
+	</header>
