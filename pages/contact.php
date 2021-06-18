abc4d5295b3337c4d611539fbfaf58ecd329ffbd
hidde <hiddealberts@gmail.com>
2021-06-18 11:02:23 +0200

    contact page

---
+++ pages/contact.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="css/contact-page.css">
+<div class="contact">
+  <div class="img">
+    <img src="images/wolken.jpeg" alt="" width="100%" height="20%">
+  </div>
+  <div class="text">
+    <p>Wij helpen u graag persoonlijk, telefonisch of per mail met uw vragen.<br>
+
+      Bezoek ons tijdens onze openingstijden:<br><br>
+
+      Dinsdag 9:00 - 12:00<br><br>
+
+      Woensdag 9:00 - 12:00<br><br>
+
+      Vrijdag 9:00 - 12:00<br><br>
+
+      Telefonisch en per mail te bereiken van 9:00 tot 15:00 op bovenstaande dagen<br>
+
+      Je kan ons telefonisch of per mail bereiken via:<br>
+
+      reisbureau.park@alfa-college.nl<br></p>
+  <div class="map"></div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d310392.8725229353!2d6.3667475546224015!3d52.57061823386614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c801c464c8c6b3%3A0xeff41e8f8f258a7d!2sAlfa-college%20Hardenberg!5e0!3m2!1snl!2snl!4v1623663495404!5m2!1snl!2snl" height="500px" width="500px" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+  </div>
+
+</div>
