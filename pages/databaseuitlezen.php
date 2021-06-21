a695a25049baf4757805689c38afa2074809ff18
hidde <hiddealberts@gmail.com>
2021-06-21 10:44:24 +0200

    adding the read function

--- pages/databaseuitlezen.php
+++ pages/databaseuitlezen.php
@@ -6,19 +6,35 @@
   </head>
   <body>
 <?php
-  $counter = 1;
+  $id = 1;
   include 'db.php';
   $db = new db();
-  $titel = $db->Read('vakanties','titel','id =' . $counter);
+  $titel = $db->Read('vakanties','titel','id =' . $id);
   echo $titel[0]["titel"];
 
-  $picMain = $db->Read('vakanties','hoofdImg','id =' . $counter);
-  var_dump($picMain);
+  $picMain = $db->Read('vakanties','hoofdImg','id =' . $id);
   echo ('<img src="images/' . $picMain[0]["hoofdImg"] . '">');
 
-  $text = $db->Read('vakanties','text','id =' . $counter);
+  $text = $db->Read('vakanties','text','id =' . $id);
   echo $text[0]["text"];
 
+  $picSub = $db->Read('vakanties','subImg','id =' . $id);
+  $picSubCount = $db->Read('vakanties','subimgcount','id =' . $id);
+
+
+  $picSubR = $picSub[0]["subImg"];
+  $picSubD = explode(" ", $picSubR);
+
+
+  $x = (int)$picSubCount[0]["subimgcount"];
+
+
+
+for ($i=0; $i < $x; $i++) {
+  echo ('<img src="images/' . $picSubD[$i] . '">');
+
+
+}
 
 
  ?>
@@ -26,7 +42,7 @@
 
 
 
-<img src="" alt="">
+
 
   </body>
 </html>
