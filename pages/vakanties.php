393bcc7f42ef3039247351fd4a1c3a38a141653b
hiddencorner <hoekhidde12@gmail.com>
2021-06-21 09:20:20 +0200

    files are downloadable on vakantie-page

--- pages/vakanties.php
+++ pages/vakanties.php
@@ -9,8 +9,8 @@
             <h2>Hoe gaan wij te werk</h2>
                 <p>Via het aanmeldformulier en/of een persoonlijk gesprek kunt u uw wensen aan ons doorgeven. Aan de hand daarvan maken wij voor u een op maat gemaakte reisvoorstel. Deze kunt u rustig doorlezen en bespreken met uw collega’s. Samen met u besprek wij het reisvoorstel, eventuele aanmerkingen kunnen dan worden besproken. Gaat u akkoord met het voorstel, dan kunnen wij u helpen met het uitvoeren van de reis. <br>
                 Wij hebben als reisbureau niet de bevoegdheid om contracten met bedrijven aan te gaan (boeken van de vakantie). Dit moet u zelf regelen!</p>
-                <a href=""><img src="images/driehoek.png" style="margin-right:10px;" width="10" class="driehoekje"><u><i>Klik hier om de informatiemap <b>NEDERLAND</b> te downloaden</i></u></a> <br>
-                <a href=""><img src="images/driehoek.png" style="margin-right:10px;" width="10" class="driehoekje"><u><i>Klik hier om de informatiemap <b>EUROPA</b> te downloaden</i></u></a>
+                <a href="files/informatiemap_nederland.docx" download><img src="images/driehoek.png" style="margin-right:10px;" width="10" class="driehoekje"><u><i>Klik hier om de informatiemap <b>NEDERLAND</b> te downloaden</i></u></a> <br>
+                <a href="files/informatiemap_europa.docx" download><img src="images/driehoek.png" style="margin-right:10px;" width="10" class="driehoekje"><u><i>Klik hier om de informatiemap <b>EUROPA</b> te downloaden</i></u></a>
                 <br><br>
             <h2>Vakantie aanvragen</h2>
                 <p>Vul hier gemakkelijk en snel een aanvraagformulier in en wij helpen jou met het uitwerken van je ultieme reis!</p>
@@ -20,7 +20,7 @@
         <br>
         <h2>Voorbeeld vakanties</h2>
         <?php
-            for ($j=0; $j < 16; $j++) {
+            for ($j=0; $j < rand(3, 19); $j++) {
                     echo "<div class='vakantie'>
                     <img src='images/testafbeelding.jpg' style='width: 100%;'></img>
                     <h2>vakantie bestemming</h2>
