04c34947a33a5a38fda0d962fbae2e428622ea78
hiddencorner <hoekhidde12@gmail.com>
2021-06-23 10:53:25 +0200

    small changes to bpv-template & mail-form
    couldn't get the mail-form to work yet :[

--- pages/bpv.php
+++ pages/bpv.php
@@ -15,10 +15,12 @@
                 <div>
                     <h2>Lijst met landen</h2>
                     <select name="cars" id="cars">
-                        <option value="volvo">Volvo</option>
-                        <option value="saab">Saab</option>
-                        <option value="opel">Opel</option>
-                        <option value="audi">Audi</option>
+                      <?php
+                          for ($i=1; $i < 90; $i++) { 
+                            echo "<option value=" . "stage" . strval($i) . ">stage " . strval($i) . "</option>";
+                          }
+                      ?>
+                        
                     </select>
                 </div>
             </div>
@@ -42,21 +44,13 @@
                 
                     <div class="slideshow-container">
                         <div class="mySlides fade">
-                          <div class="numbertext">1 / 3</div>
                           <img src="images/header-home.jpg" style="width:100%">
-                          <div class="text">Caption Text</div>
                         </div>
-
                         <div class="mySlides fade">
-                          <div class="numbertext">2 / 3</div>
                           <img src="images/vliegtuig.jpg" style="width:100%">
-                          <div class="text">Caption Two</div>
                         </div>
-
                         <div class="mySlides fade">
-                          <div class="numbertext">3 / 3</div>
                           <img src="images/loc-gebouw-map.jpg" style="width:100%">
-                          <div class="text">Caption Three</div>
                         </div>
 
                         <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
