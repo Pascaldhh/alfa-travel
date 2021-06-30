4cbb48d64ffb4ddbcac7641e30c0be6994c70788
hiddencorner <hoekhidde12@gmail.com>
2021-06-30 13:07:07 +0200

    klassenveranderingen toegevoegd

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 	</div>
 	<footer>
-		<div class="normal-foot"></div>
+		<div class="normal-foot">
+			<p class="FootMain" style="padding-right: 15px;">© Alfa-college | © Alfa travel | bel ons op 06-0883342264 of stuur een mailtje naar reisbureau.park@alfa-college.nl</p>
+		</div>
 		<div class="rotate-foot"></div>
 	</footer>
 	<script src="main.js"></script>  
--- pages/vakanties.php
+++ pages/vakanties.php
@@ -1,6 +1,8 @@
 <link rel="stylesheet" type="text/css" href="css/vakantie-page.css">
     <div class="container">
-        <block>
+        <div class="header-container">
+          <img src="images/vakantieBanner.png" alt="" class="header-home">
+        </div>
         <div class="left-half">
             <div class="vierkantAchterText">
             <h2><?php echo $db->Read('website_content', 'content', 'page_id = "4" AND id = "21"')[0]['content'];?></h2>
@@ -13,6 +15,10 @@
             <h2><?php echo $db->Read('website_content', 'content', 'page_id = "4" AND id = "27"')[0]['content'];?></h2>
                 <p><?php echo $db->Read('website_content', 'content', 'page_id = "4" AND id = "28"')[0]['content'];?></p>
                 <a href="?page=formulier"><img src="images/driehoek.png" style="margin-right:10px;" width="10" class="driehoekje"><u><i><?php echo $db->Read('website_content', 'content', 'page_id = "4" AND id = "29"')[0]['content'];?></i></u></a>
+                <div style="display: flex; justify-content: flex-end; margin-top: 50px;">
+                    <h3 style="margin: 25px 10px;">Hebt u nog vragen?</h3>
+                    <a href="tel:+31-0883342264"><div class="button"><p>Klik hier om te bellen!</p></div></a>
+                </div>
             </div>
         </div>
         <br>
